817889ea69fb70d4e0bf321dbb906ac7e4e7265d
mshakir082 <mdshakirhussain298@gmail.com>
2022-07-28 22:25:00 +0530

    Historical product page

--- src/App.js
+++ src/App.js
@@ -1,22 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
 import Navbar from './Components/Navbar';
-import Header from './Components/Header';
-import {SimpleSlider} from './Components/SimpleSlider';
-import FeaturedCollections from './Components/FeaturedCollections';
-import Months from './Components/Months';
 import Footer from './Components/Footer';
+import MainRoutes from './Router/MainRoutes'
 function App() {
   return (
     <div className="App">
       <Navbar/>
       <div style={{width:'75%',height:'auto',margin:'auto'}}>
-        <Header/>
-        <SimpleSlider/>
-        <FeaturedCollections/>
-        <Months/>
-       
-      </div>
+      <MainRoutes/>
+       </div>
       <Footer/>
     </div>
   );
